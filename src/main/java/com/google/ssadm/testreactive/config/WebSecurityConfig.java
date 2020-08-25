d603981935c95f0eb7f0f06ed7b235264d2586c6
Oleksandr <ssadmin@gmail.com>
2020-08-25 13:04:49 +0300

    add JWT

--- src/main/java/com/google/ssadm/testreactive/config/WebSecurityConfig.java
+++ src/main/java/com/google/ssadm/testreactive/config/WebSecurityConfig.java
@@ -1,16 +1,29 @@
 package com.google.ssadm.testreactive.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class WebSecurityConfig {
+
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
+
+    @Autowired
+    public WebSecurityConfig(AuthenticationManager authenticationManager, SecurityContextRepository securityContextRepository) {
+        this.authenticationManager = authenticationManager;
+        this.securityContextRepository = securityContextRepository;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -20,9 +33,25 @@ public class WebSecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
         return httpSecurity
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
+                                )
+                )
+                .accessDeniedHandler(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
+                                )
+                )
+                .and()
                 .csrf().disable()
-                .formLogin().and()
+                .formLogin().disable()
                 .httpBasic().disable()
+                .authenticationManager(authenticationManager)
+                .securityContextRepository(securityContextRepository)
                 .authorizeExchange()
                 .pathMatchers("/", "/login", "/user", "/favicon.ico").permitAll()
                 .pathMatchers("/controller", "/admin").hasRole("ADMIN")
--- src/main/java/com/google/ssadm/testreactive/controllers/UserController.java
+++ src/main/java/com/google/ssadm/testreactive/controllers/UserController.java
@@ -1,2 +1,48 @@
-package com.google.ssadm.testreactive.controllers;public class UserController {
+package com.google.ssadm.testreactive.controllers;
+
+import com.google.ssadm.testreactive.config.JwtUtil;
+import com.google.ssadm.testreactive.domain.User;
+import com.google.ssadm.testreactive.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@RestController
+public class UserController {
+
+    private final static ResponseEntity<Object> UNAUTHORIZED =
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public UserController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public Mono<ResponseEntity> login(ServerWebExchange swe){
+        return swe.getFormData().flatMap(credentials ->
+              userService.findByUsername(credentials.getFirst("username"))
+                      .cast(User.class)
+                      .map(userDetails ->
+                              Objects.equals(
+                                      credentials.getFirst("password") ,
+                                      userDetails.getPassword()
+                              )
+                                      ? ResponseEntity.ok(jwtUtil.generateToken(userDetails))
+                                      : UNAUTHORIZED
+                      )
+                      .defaultIfEmpty(UNAUTHORIZED)
+        );
+
+    }
 }
